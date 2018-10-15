eb19ff20d875e2e08705b80ea5bfcad65159b2e8
LiangGongxun <lianggx@jyasset.com.cn>
2018-10-15 14:20:10 +0800

    update

--- DatetimeTest.py
+++ DatetimeTest.py
@@ -1,19 +1,19 @@
 from datetime import datetime
 
 
-# s = "%Y-%m-%dT%H:%M:%S.%f+08:00"
-# t = "2018-10-09T11:22:38.000+08:00"
-# date1 = datetime(2018, 7, 11)
-# date2 = datetime(2018, 7, 12)
-# date3 = datetime(2018, 7, 13)
-# date4 = datetime(2018, 7, 14)
-# date5 = datetime(2018, 7, 15)
-# print(date1.timestamp())
-# print(date2.timestamp())
-# print(date3.timestamp())
-# print(date4.timestamp())
-# print(date5.timestamp())
+s = "%Y-%m-%dT%H:%M:%S.%f+08:00"
+t = "2018-10-09T11:22:38.000+08:00"
+date1 = datetime(2018, 7, 11)
+date2 = datetime(2018, 7, 12)
+date3 = datetime(2018, 7, 13)
+date4 = datetime(2018, 7, 14)
+date5 = datetime(2018, 7, 15)
+print(date1.timestamp())
+print(date2.timestamp())
+print(date3.timestamp())
+print(date4.timestamp())
+print(date5.timestamp())
 
 
-date = datetime.fromtimestamp(1531288326.192)
-print(date)
+# date = datetime.fromtimestamp(1531353600.226)
+# print(date)
--- SPRebuild.py
+++ SPRebuild.py
@@ -13,31 +13,23 @@ def sp_rebuild(transfer_date):
     old_collection = old_database.sp
     start_time = datetime.strptime(transfer_date, "%Y-%m-%d").timestamp()
     end_time = (datetime.strptime(transfer_date, "%Y-%m-%d") + timedelta(days=1)).timestamp()
-    my_filter = {"$and": [{"Timestamp": {"$gte": start_time}},
-                          {"Timestamp": {"$lte": end_time}}]}
-    missing_document = 0
-    total_document = 0
-    new_document = 0
-    print("The transfer of %s begin" % transfer_date)
-    for result in old_collection.find(my_filter).batch_size(10000):
-        total_document += 1
-        if "t" in result:
-            matched_count = tick.count_documents({"$and": [{"code": result["code"]},
-                                                           {"t": result["t"]}]})
-            if matched_count == 0:
-                tick.insert_one(result)
-                new_document += 1
-        else:
-            missing_document += 1
-    print("The number of total document is %d" % total_document)
-    print("The number of missing document is %d" % missing_document)
-    print("The number of new document is %d" % new_document)
-    print("The transfer is end")
+    my_filter = {"$and": [{"t": {"$gte": start_time}},
+                          {"t": {"$lte": end_time}}]}
+    print(str(datetime.now()) + " The transfer of %s begin" % transfer_date)
+    document_buffer = []
+    for result in old_collection.find(my_filter, {"_id": 0}).batch_size(10000):
+        if result not in document_buffer:
+            if len(document_buffer) >= 10:
+                tick.insert_one(document_buffer.pop(0))
+            document_buffer.append(result)
+    for buf in document_buffer:
+        tick.insert_one(buf)
+    print(str(datetime.now()) + " The transfer is end")
 
 
 if __name__ == "__main__":
     start_date = datetime(2018, 7, 11)
-    end_date = datetime(2018, 10, 9)
+    end_date = datetime(2018, 7, 11)
     for i in range((end_date - start_date).days + 1):
         date = start_date + timedelta(days=i)
         sp_rebuild(date.strftime("%Y-%m-%d"))
